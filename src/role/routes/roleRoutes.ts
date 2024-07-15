8be820cbc7b33c445cb67c52297f9e5a99a183da
Somer <233424@ids.upchiapas.edu.mx>
2024-07-15 14:38:51 -0600

    Se ha añadido seguridad a el CRUD de Role

--- src/role/routes/roleRoutes.ts
+++ src/role/routes/roleRoutes.ts
@@ -4,10 +4,10 @@ import { createRole, deletedRoleLogic, deleteRole, getRoleById, getRoles, update
 
 const rolesRoutes: Router = Router();
 
-rolesRoutes.get('/',getRoles);
-rolesRoutes.get('/:product_id',getRoleById);
-rolesRoutes.post('/', createRole);
-rolesRoutes.put('/:product_id', updatedRole);
+rolesRoutes.get('/',authMiddleware,getRoles);
+rolesRoutes.get('/:product_id',authMiddleware,getRoleById);
+rolesRoutes.post('/',authMiddleware, createRole);
+rolesRoutes.put('/:product_id', authMiddleware, updatedRole);
 rolesRoutes.delete('/:product_id',authMiddleware, deleteRole);
-rolesRoutes.put("/delete/:product_id", deletedRoleLogic);
+rolesRoutes.put("/delete/:product_id", authMiddleware, deletedRoleLogic);
 export default rolesRoutes;
