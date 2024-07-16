4bdbcde449e70a6f18566644ac313eefab9a0d25
Somer <233424@ids.upchiapas.edu.mx>
2024-07-16 08:15:35 -0600

    He regresado el código al último commit anterior

--- src/reservation/repositories/reservationRepositories.ts
+++ src/reservation/repositories/reservationRepositories.ts
@@ -34,7 +34,7 @@ export class ReservationRepository {
   }
   public static async findByStatusAcepted(): Promise<Reservation[]> {
     return new Promise((resolve, reject) => {
-      connection.query('SELECT * FROM Reservation WHERE status = Aceptado', (error: any, results) => {
+      connection.query('SELECT * FROM Reservation WHERE status = "Aceptado"', (error: any, results) => {
         if (error) {
           reject(error);
         } else {
@@ -46,7 +46,7 @@ export class ReservationRepository {
   }
   public static async findByStatusPending(): Promise<Reservation[]> {
     return new Promise((resolve, reject) => {
-      connection.query('SELECT * FROM Reservation WHERE status = Pendiente', (error: any, results) => {
+      connection.query('SELECT * FROM Reservation WHERE status = "Pendiente"', (error: any, results) => {
         if (error) {
           reject(error);
         } else {
