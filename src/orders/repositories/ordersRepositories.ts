3891bf0685f98809f9e9e61ef8ae6c871e6c0a07
Somer <233424@ids.upchiapas.edu.mx>
2024-07-17 11:20:02 -0600

    token

--- src/orders/repositories/ordersRepositories.ts
+++ src/orders/repositories/ordersRepositories.ts
@@ -1,7 +1,7 @@
 import { ResultSetHeader } from 'mysql2';
 import connection from '../../shared/config/database';
 import { Orders } from '../models/ordersModel';
-import { ProductOrders } from '../models/productId';
+//import { ProductOrders } from '../models/productId';
 export class OrdersRepository {
 
   public static async findAll(): Promise<Orders[]> {
