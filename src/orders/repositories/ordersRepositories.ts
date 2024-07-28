7d0b707f811885e188ed22b6c9d9d932cc84b853
jesus-imanol <jesusimanolcastillo@gmail.com>
2024-07-28 01:06:52 -0600

    apostrophe

--- src/orders/repositories/ordersRepositories.ts
+++ src/orders/repositories/ordersRepositories.ts
@@ -36,7 +36,7 @@ export class OrdersRepository {
   }
   public static async findOrdersWithProducts(order_date: Date): Promise<ProductWithOrdersAndUser[]>{
     return new Promise((resolve, reject)=>{
-      connection.query("SELECT orders_id, product_id, user_id, ProductOrders.amount, order_date, total_amount, status,description, price, name, lastname, email, number_phone FROM Orders JOIN User On user_id=user_id_fk JOIN ProductOrders ON orders_id= orders_id_fk JOIN Product ON product_id=product_id_fk WHERE order_date=? AND status = Vendido,  Orders.deleted=0 AND Product.deleted = 0 AND User.deleted =0 ORDER BY orders_id ", [order_date], (error: any, results)=>{
+      connection.query("SELECT orders_id, product_id, user_id, ProductOrders.amount, order_date, total_amount, status,description, price, name, lastname, email, number_phone FROM Orders JOIN User On user_id=user_id_fk JOIN ProductOrders ON orders_id= orders_id_fk JOIN Product ON product_id=product_id_fk WHERE order_date=? AND status = Vendido Orders.deleted=0 AND Product.deleted = 0 AND User.deleted =0 ORDER BY orders_id ", [order_date], (error: any, results)=>{
         if(error){
           reject(error);
         }else{
