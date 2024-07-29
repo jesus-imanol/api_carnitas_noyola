a95a5be8a950a16b118e4f4876fd61dcd4cadfc5
jesus-imanol <jesusimanolcastillo@gmail.com>
2024-07-29 00:58:26 -0600

    token no

--- src/products/routes/productsRoutes.ts
+++ src/products/routes/productsRoutes.ts
@@ -4,12 +4,12 @@ import { getProducts, getProductById, createProduct, updatedProduct, deleteProdu
 import upload from '../../shared/middlewares/uploadMidleware';
 import { authMiddleware } from '../../shared/middlewares/auth';
 const productsRoutes: Router = Router();
-productsRoutes.get('/product/', authMiddleware, getProducts);
+productsRoutes.get('/product/', getProducts);
 productsRoutes.get('/byRole/:role_id', authMiddleware, getProductById);
 productsRoutes.post('/',authMiddleware, upload.single('image'),createProduct);
 productsRoutes.put('/product/:product_id', authMiddleware, updatedProduct);
 productsRoutes.delete('/:product_id', authMiddleware, deleteProduct);
-productsRoutes.get("/productDescription/:description", authMiddleware, getProductsByDescription)
+productsRoutes.get("/productDescription/:description", getProductsByDescription)
 productsRoutes.put("/delete/:product_id", authMiddleware, deletedProductLogic);
 productsRoutes.put("/addAmount/:product_id", authMiddleware, addAmountProduct);
 export default productsRoutes;
